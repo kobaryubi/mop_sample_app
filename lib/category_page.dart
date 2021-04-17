dbce6889d51d50c844888f83414cdc49db84f72e
MasahikoKobayashi <kobaryubi6@gmail.com>
2021-04-17 21:56:00 +0900

    improved category_page.dart

--- lib/category_page.dart
+++ lib/category_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
 class CategoryPage extends StatelessWidget {
   final String name;
   final ColorSwatch color;
@@ -14,6 +17,46 @@ class CategoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      body: Center(
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            height: _rowHeight,
+            child: InkWell(
+              borderRadius: _borderRadius,
+              highlightColor: color,
+              splashColor: color,
+              onTap: () {
+                print("I was tapped!");
+              },
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(
+                        iconLocation,
+                        size: 60.0,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        name,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
