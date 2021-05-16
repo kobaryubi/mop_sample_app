1c3702b2e4e06bdd848d9c2c9880c51201cc6d59
MasahikoKobayashi <kobaryubi6@gmail.com>
2021-05-16 23:47:53 +0900

    improved gesture_route.dart

--- lib/gesture_route.dart
+++ lib/gesture_route.dart
@@ -8,6 +8,8 @@ class GestureRoute extends StatefulWidget {
 }
 
 class _GestureRouteState extends State<GestureRoute> {
+  bool _lights = false;
+
   @override
   void initState() {
     super.initState();
@@ -19,7 +21,51 @@ class _GestureRouteState extends State<GestureRoute> {
       appBar: AppBar(
         title: Text('Gesture Route'),
       ),
-      body: Text('Gesture Route'),
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.lightbulb_outline,
+                      color: _lights ? Colors.yellow.shade600 : Colors.black,
+                      size: 60,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _lights = !_lights;
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: Text(_lights ? 'TURN LIGHT OFF' : 'TURN LIGHT ON'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
