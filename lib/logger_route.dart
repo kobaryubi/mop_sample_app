2c3403aaf58899056ece568e1ea2d72d623e6cfa
MasahikoKobayashi <kobaryubi6@gmail.com>
2021-05-23 16:37:34 +0900

    commit

--- lib/logger_route.dart
+++ lib/logger_route.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+final logger = new Logger(
+  printer: PrettyPrinter(),
+);
+
+final loggerNoStack = Logger(
+  printer: PrettyPrinter(methodCount: 0),
+);
 
 class LoggerRoute extends StatefulWidget {
   const LoggerRoute();
@@ -11,6 +20,19 @@ class _LoggerRouteState extends State<LoggerRoute> {
   @override
   void initState() {
     super.initState();
+
+    logger.v('Verbose log');
+    loggerNoStack.v('Verbose log');
+    logger.d('Debug log');
+    loggerNoStack.d('Debug log');
+    logger.i('Info log');
+    loggerNoStack.i('Info log');
+    logger.w('Warning log');
+    loggerNoStack.w('Warning log');
+    logger.e('Error log');
+    loggerNoStack.e('Error log');
+    logger.wtf('What a terrible failure log');
+    loggerNoStack.wtf('What a terrible failure log');
   }
 
   @override
@@ -22,11 +44,7 @@ class _LoggerRouteState extends State<LoggerRoute> {
       body: Center(
         child: Container(
           color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              Text('TEST'),
-            ],
-          ),
+          child: Text('Logger'),
         ),
       ),
     );
